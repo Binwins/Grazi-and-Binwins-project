8ad00a268e22331089f266743fab7054bb5c617a
Binwins <dpblanchette@gmail.com>
2013-07-30 14:50:28 -0400

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?PHP
+
+  // Copyright Daniel Blanchette
+
+	if(!defined('IN_SITE'))
+		die("Acess Denied");
+		
+	$mysql_host = "localhost";
+	$mysql_db = "insert";
+	$mysql_user = "insert";
+	$mysql_pass =  "insert";
+	$table_prefix = "insert";
+	
+?>
